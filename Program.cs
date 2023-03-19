659ab55ea552ba39f9879da5c2688831fd77b3f4
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2023-03-19 09:55:13 -0300

    Criada a solução 1001

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace beecrowd1001
+{
+    class Program
+    {
+        static void Main()
+        {
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+
+        }
+    }
+}
